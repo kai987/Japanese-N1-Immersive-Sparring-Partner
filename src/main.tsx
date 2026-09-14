1397ee4c864ef1ed9c9c1cf54eb63d5d5e5d2b9e
Kai <65030330+kai987@users.noreply.github.com>
2026-09-14 23:58:48 +0900

    style: load hero title override

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './readability.css'
 import './search.css'
+import './hero-title.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
